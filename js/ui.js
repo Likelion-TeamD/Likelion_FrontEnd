0f0872a9bb7dadf0f1a7e833bced676e3ea09c35
chanu <cw8881@naver.com>
2023-08-03 02:10:11 +0900

    햄버거메뉴 스크롤 방법

--- js/ui.js
+++ js/ui.js
@@ -3,15 +3,12 @@
 //     alert('클릭')
 // })
 $(document).on('click', '#menuButton',function (){
-    alert('햄버거 메뉴 나오기');
     $('#hamburgerMenu').show()
 })
 
 // html id 가 있어야 가능
 $(document).on('click', '#hamburgerCloseButton',function (){
     $('#hamburgerMenu').hide();
-    
-    alert('햄버거 메뉴 나오기');
 })
 
 
