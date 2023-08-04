3c3e008984d50a531b3a05145a1d48ab4ecdc4de
chanu <cw8881@naver.com>
2023-08-04 20:00:20 +0900

    header 및 footer 외부 html사용

--- js/ui.js
+++ js/ui.js
@@ -2,14 +2,14 @@
 // $('html').click(function () {
 //     alert('클릭')
 // })
-// $(document).on('click', '#menuButton', function () {
-//     $('#hamburgerMenu').show()
-// })
+$(document).on('click', '#menuButton', function () {
+    $('#hamburgerMenu').show()
+})
 
-// // html id 가 있어야 가능
-// $(document).on('click', '#hamburgerCloseButton', function () {
-//     $('#hamburgerMenu').hide();
-// })
+// html id 가 있어야 가능
+$(document).on('click', '#hamburgerCloseButton', function () {
+    $('#hamburgerMenu').hide();
+})
 
 var menuButton = document.getElementById("menuButton");
 menuButton.addEventListener("click",toggleHamburgerMenu);
