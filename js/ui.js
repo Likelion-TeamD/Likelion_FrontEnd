45bf2a3f66753c055037423b23c8d43c68bffa85
chanu <cw8881@naver.com>
2023-08-08 17:14:19 +0900

    header 정리

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,27 @@
-// Initialization for ES Users
-import {
-    Collapse,
-    Dropdown,
-    initTE,
-  } from "tw-elements";
+$(document).ready(function () {
+  var id = ['#home', '#guest', '#sale']
+
+  // Get the current URL
+  const currentUrl = window.location.pathname;
+
+  // Highlight the current page in the navbar
+
   
-  initTE({ Collapse, Dropdown });
+  if (currentUrl === '/index.html') {
+    changeColor(id[0])
+  }else if (currentUrl === '/pages/guest.html') {
+    changeColor(id[1])
+  }else if (currentUrl === '/pages/sale.html') {
+    changeColor(id[2])
+  }
+  
+});
+
+function changeColor(id) {
+  document.querySelector(id).style.color = "#29A99A";
+  
+  // const colorDiv = $(id);
+  // colorDiv.css(".color-custom-click");
+  console.log(1)
+}
+
